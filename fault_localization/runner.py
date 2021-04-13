0fc5decc22bef873e69302f3969b72b617621c39
zosoist <moh.robati1333@gmail.com>
2021-04-14 02:41:38 +0430

    refactored project structure

---
+++ fault_localization/runner.py
@@ -0,0 +1,76 @@
+import subprocess
+import json
+import ast
+
+
+class Runner:
+
+    def __init__(self, fileName, preprocessedCode):
+        self.__fileName = fileName
+        self.__preprocessedCode = preprocessedCode
+        self.__preprocessedCodePath = './sample_codes/preprocessed/' + fileName + '_preprocessed.js'
+        self.__tempCode = self.__getTempCode('./configs/input_reader.txt')
+        self.__testCases = self.__getTestCases('./sample_codes/test_cases/'+fileName+'_test_cases.json')
+        self.__OKGREEN = '\033[92m'
+        self.__FAIL = '\033[91m'
+        self.__ENDC = '\033[0m'
+
+    def __getTempCode(self, path):
+        file = open(path)
+        tempCode = file.read()
+        file.close()
+        return tempCode
+
+    def __getTestCases(self, path):
+        file = open(path)
+        testCases = json.loads(file.read())
+        file.close()
+        return testCases
+
+    def __getInputsOutputs(self, test):
+        inputs = ""
+        for input in test['input']:
+            inputs += input + '\n'
+        return inputs, test['output']
+
+    def __writePreprocessedCode(self, fileName, code):
+        file = open(self.__preprocessedCodePath, "w+")
+        file.write(code)
+        file.close()
+        return self.__preprocessedCodePath
+
+    def __executeCommant(self, path, test):
+        result = subprocess.run(['node', path], stdout=subprocess.PIPE).stdout.decode('utf-8').replace(
+            "\n", "")
+        first = result.find("%%locs")
+        second = result.rfind("%%locs")
+        predictedValue = result[:first].replace(" ", "")
+        locs = ast.literal_eval(result[first + len("%%locs"):second].replace(" ", ""))
+        evaluation = predictedValue == str(test['output'][0])
+        return predictedValue, locs, evaluation
+
+    def __evaluateTest(self, evaluation):
+        if evaluation:
+            msg = "PASSED"
+            color = self.__OKGREEN
+        else:
+            msg = "FAILED"
+            color = self.__FAIL
+        return msg, color
+
+    def run(self):
+        counter = 1
+        for test in self.__testCases:
+            inputs, output = self.__getInputsOutputs(test)
+            code = self.__tempCode.replace("%%inputs", inputs)
+            code = code.replace("%%code", self.__preprocessedCode)
+            path = self.__writePreprocessedCode(self.__fileName, code)
+            predictedValue, locs, evaluation = self.__executeCommant(path, test)
+            msg, color = self.__evaluateTest(evaluation)
+            print(color + 'TestCase #'+str(counter)+': --', msg)
+            print("Real Value:", test['output'][0])
+            print("Predicted Value:", output)
+            print("Locations:", locs, self.__ENDC)
+            print()
+            counter += 1
+
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import esprima
 from fault_localization.preprocessor import Preprocessor
-import subprocess
-import json
-import ast
+from fault_localization.runner import Runner
 
-OKGREEN = '\033[92m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-
-fileName = 'main'
-inputPath = 'js_tests/' + fileName + ".js"
+fileName = 'sample_01'
+inputPath = 'sample_codes/' + fileName + ".js"
 file = open(inputPath)
 program = file.read()
 file.close()
@@ -17,43 +11,5 @@ file.close()
 preprocessor = Preprocessor(program)
 parsedCode = esprima.parseScript(program, delegate=preprocessor.preprocess)
 preprocessedCode = preprocessor.getPreprocessedCode()
-
-file = open('configs/input_reader.txt')
-temp_code = file.read()
-file.close()
-
-file = open('js_tests/preprocessed/data.json')
-io = json.loads(file.read())
-file.close()
-
-counter = 1
-for test in io:
-    inputs = ""
-    for input in test['input']:
-        inputs += input + '\n'
-    code = temp_code.replace("%%inputs", inputs)
-    code = code.replace("%%code", preprocessedCode)
-    preprocessedCodePath = 'js_tests/preprocessed/' + fileName + '_preprocessed.js'
-    file = open(preprocessedCodePath, "w+")
-    file.write(code)
-    file.close()
-    result = subprocess.run(['node', preprocessedCodePath], stdout=subprocess.PIPE).stdout.decode('utf-8').replace("\n", "")
-    first = result.find("%%locs")
-    second = result.rfind("%%locs")
-    output = result[:first].replace(" ", "")
-    locs = ast.literal_eval(result[first+len("%%locs"):second].replace(" ", ""))
-    evaluation = output == str(test['output'][0])
-    msg, color = "", ""
-    if evaluation:
-        msg = "PASSED"
-        color = OKGREEN
-    else:
-        msg = "FAILED"
-        color = FAIL
-    print(color + 'TestCase #'+str(counter)+': --', msg)
-    print("Real Value:", test['output'][0])
-    print("Predicted Value:", output)
-    print("Locations:", locs, ENDC)
-    print()
-    counter += 1
+Runner(fileName, preprocessedCode).run()
 
